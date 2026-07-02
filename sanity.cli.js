e784b6ca31f8c9275c6b7ebf248868d90146e789
Camden Harkins <camdenharkins24@gmail.com>
2026-07-02 00:57:20 -0400

    Add Sanity CLI config for Studio deploys

---
+++ sanity.cli.js
@@ -0,0 +1,11 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'meyw672s',
+    dataset: 'production',
+  },
+  deployment: {
+    appId: 'h6iru7a8d5ee29000cip9ylh',
+  },
+})
